a9369cace1a2812ec63961587066aabf8cf1bfe2
jadsonecomp <jadsonecomp@gmail.com>
2020-06-29 17:01:26 -0300

    início da inserção do bando de dados sqllite na aplicação

---
+++ src/database/db.js
@@ -0,0 +1,78 @@
+//importar a dependência do sqlite 3
+const sqllite3 = require("sqlite3").verbose()  //verbose indica ao terminal que quero ver mensagens do banco
+
+//criar o objeto que irá fazer operações no banco de dados
+const db = new sqllite3.Database("./src/database/database.db")
+
+module.exports = db
+
+//comentário em bloco crtl + ;
+//utilizar  o objeto de banco de dados para nossos operações
+// db.serialize( () => {
+//     //Com comandos sql:
+
+//     //1 - criar tabelas
+//     db.run(`
+//         CREATE TABLE IF NOT EXISTS places(
+//             id INTEGER PRIMARY KEY AUTOINCREMENT,
+//             image TEXT,
+//             name TEXT,
+//             address TEXT,
+//             address2 TEXT,
+//             state TEXT,
+//             city TEXT,
+//             items TEXT        
+//         );
+//     `)
+
+//     //2 - inserir dados
+//     const query = `
+//         INSERT INTO places(
+//             image,
+//             name,
+//             address,
+//             address2,
+//             state,
+//             city,
+//             items     
+//             ) VALUES(?,?,?,?,?,?,?);
+//         `     
+//     const values =  [
+//         "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+//          "Papersider",
+//          "Guilherme Gembala, Jardim América",
+//          "N° 260",
+//          "Santa Catarina",
+//          "Rio do Sul",
+//          "Resíduos Eletrônicos, Lâmpadas"    
+//         ]  
+    
+    
+//     function afterInsertData(err){
+//         if(err){
+//             return console.log(err)
+//         }
+//         console.log("Cadastrado com sucesso")
+//         console.log(this)
+//     }
+//     //descomentar se quiser inserir mais dados
+//     db.run(query, values, afterInsertData)  //a chamada assim afterInsertData, estou passando por referencia, a chamada afterInsertData() estou executando a função diretamente
+
+//     //3 - consultar dados tabela
+//     db.all(`SELECT * FROM places`, function(err, rows){
+//         if(err){
+//             return console.log(err)
+//         }
+//         console.log("Aqui estão seu registros")
+//         console.log(rows)    
+//     })
+
+//     //4 - deletar dados tabela
+//     db.run(`DELETE FROM places WHERE id = ?`, [1], function(err){
+//         if(err){
+//             console.log(err)
+//         }
+//         console.log("Registro deletado com sucesso")
+//     })
+    
+// })
--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@
 const express = require("express")
 const server = express()
 
+//pegar o banco de dados
+const db = require("./database/db")
+
 //configurar pasta pública
 server.use(express.static("public"))
 
@@ -29,7 +32,19 @@ server.get("/create-point", (req, res) => {
 })
 
 server.get("/search", (req, res) => {
-    return res.render("search-results.html")
+
+    //pegar os dados do banco de dados
+    db.all(`SELECT * FROM places`, function(err, rows){
+            if(err){
+                return console.log(err)
+            }
+            console.log("Aqui estão seu registros")
+            console.log(rows)    
+
+            //mostrar a página html com os dados do banco de dados
+            return res.render("search-results.html", {places: rows})
+        }) 
+
 })
 
 
